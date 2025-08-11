bf8eecd974744bae02e842a5fdbfe25762cdbf01
Anup Kumar <110590496+Anup64@users.noreply.github.com>
2025-08-11 11:33:37 +0530

    Add files via upload

---
+++ Spring-Boot_Ws/movie-customer-service/src/main/java/com/example/movie_customer_service/MovieCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.movie_customer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCustomerServiceApplication.class, args);
+	}
+
+}
