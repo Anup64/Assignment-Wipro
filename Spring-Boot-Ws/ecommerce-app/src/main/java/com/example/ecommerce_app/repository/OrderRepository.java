0c28b58ffdbccd389938e5c8334455b30ad3c0f8
Anup Kumar <110590496+Anup64@users.noreply.github.com>
2025-08-11 11:26:51 +0530

    Delete Spring-Boot-Ws/ecommerce-app directory

--- Spring-Boot-Ws/ecommerce-app/src/main/java/com/example/ecommerce_app/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerce_app.repository;
-
-import com.example.ecommerce_app.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- Spring-Boot-Ws/ecommerce-app/src/main/java/com/example/ecommerce_app/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerce_app.repository;
-
-import com.example.ecommerce_app.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
